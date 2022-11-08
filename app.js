97a2d60a4efb40741ff074d89d91ebef64b8c878
Skylar Ramsey <skylarramsey362@yahoo.com>
2022-11-07 19:09:05 -0500

    created resume template with styling

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.set('view engine', 'ejs');
 //set views file that express looks for to render ejs files to the path
 //of the directory of this file in the subfolder called views
 app.set('views', path.join(__dirname, 'views'));
+//allow app to pull from public directory
+app.use(express.static(__dirname + '/public'));
 //app.get methods
 //render file given on get request
 app.get('/', function(req, res){
